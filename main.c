263da29967404d24a3b9f65030dac0ccd15382cf
Ignacio Antonio Reyes Toledo <45189062+IgnacioGameolay@users.noreply.github.com>
2024-06-18 23:55:24 +0000

    Modifying main()

--- main.c
+++ main.c
@@ -8,10 +8,7 @@
 #include <string.h>
 
 
-typedef struct{
-  char nombre[30];
-  int edad;
-} waka;
+
 
 /**
  *
@@ -21,38 +18,67 @@ typedef struct{
 int main() 
 { 
 	
-	int chipCount = 10000;
 	puts("========================================");
 	puts("     Cantidad Actual de Fichas:\n");
 	printf("%d\n", chipCount);
 	puts("========================================");
 	
-	
+	int chipCount = 10000;
 	char option; //Option del menu
 	do {
 		puts("========================================");
-		puts("     Escoge una opci[on");
+		puts("     Bienvenido al Casino Virtual IDL");
 		puts("========================================");
 		
-		puts("1) Búsqueda en Profundidad");
-		puts("4) Salir");
-	
+		puts("(1) Blackjack (Apuesta mínima $25)");
+		puts("(2) Poker (Apuesta mínima $25)");
+		puts("(3) Higher or Lower  (Apuesta mínima $100)");
+		puts("(4) Ruleta (Apuesta mínima $25)");
+		puts("(5) Jackpot (Apuesta mínima $25)");
+		puts("(6) Craps (Apuesta mínima $50)");
+
+		puts("(8) Guardar Progreso");
+		puts("(9) Cargar Progreso");
+		puts("(0) Salir del Casino");
+		
+		puts("========================================");
 		printf("Ingrese su opción: ");
+		puts("========================================");
 		scanf(" %c", &option);
-	
+
+
+		//Opciones del menu
+		
 		switch (option) {
 		case '1':
+			blackjack(chipCount);
 			break;
 		case '2':
+			poker(chipCount);
 			break;
-			
 		case '3':
+			higherOrLower(chipCount);
+			break;
+		case '4':
+			ruleta(chipCount);
+			break;
+		case '5':
+			jackpot(chipCount);
+			break;
+		case '6':
+			craps(chipCount);
+			break;
+		case '8':
+			saveProgress(chipCount);
+			break;
+		case '9':
+			loadProgress(chipCount);
 			break;
 		}
 		presioneTeclaParaContinuar();
 		limpiarPantalla();
 
-	} while (option != '4');
+	} while (option != '0');
 
 	return 0;
 }
